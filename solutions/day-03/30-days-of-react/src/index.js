91b18fb134c371c430f169a3b62c2e148f189ed8
ajhoffmeister <aidan.james.hoffmeister@gmail.com>
2025-08-12 15:16:37 -0400

    Day 03 - Basic HTML in React

--- solutions/day-03/30-days-of-react/src/index.js
+++ solutions/day-03/30-days-of-react/src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Header Style and Element
+
+const headerStyles = {
+  backgroundColor: '#FF739B',
+  color: '#33113B',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+
+const header = (
+  <header style={headerStyles}>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Instructor: Asabeneh Yetayeh</p>
+      <p>Student: Aidan 'James' Hoffmeister</p>
+      <small>Date: Aug 12, 2025</small>
+    </div>
+  </header>
+)
+
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+
+// CSS styles
+const mainStyles = {
+  backgroundColor: '#FF99AC',
+}
+
+// JSX element, main
+const main = (
+  <main style={mainStyles}>
+    <div className='main-wrapper'>
+      <p>
+        Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+    </div>
+  </main>
+)
+
+// footer styles
+// CSS styles
+const footerStyles = {
+  backgroundColor: '#FF99AC',
+}
+
+// JSX element, footer
+const footer = (
+  <footer style={footerStyles}>
+    <div className='footer-wrapper'>
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+
+// JSX element, app
+const appStyles = {
+  backgroundColor: '#FFFFFF',
+}
+const app = (
+  <div style={appStyles} className='app'>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const root = ReactDOM.createRoot(rootElement)
+root.render(app)
